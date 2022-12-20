5f2163dda3d3d2d6744d530053ae2a2ee60a71be
sukmalo <114556628+sukmalo@users.noreply.github.com>
2022-12-20 14:52:35 +0800

    Update sensor_task_1_2.py

--- mqtt-master/sensor_task_1_2.py
+++ mqtt-master/sensor_task_1_2.py
@@ -4,7 +4,7 @@ import paho.mqtt.client as mqtt_client
 from statistics import mean
 import serial
 
-my_id = 618
+my_id = 310
 values = []
 initial = True
 
@@ -29,11 +29,11 @@ while True:
     if ser.in_waiting > 0:
         data = ser.read(1)
         print(data[0])
-        client.publish("lab/%s/photo/instant" % my_id, data[0])
+        client.publish("lab/photo/instant" % my_id, data[0])
         if initial:
             values = [data[0] for i in range(100)]
             initial = False
         values.pop(0)
         values.append(data[0])
-        client.publish("lab/%s/photo/averge" % my_id, mean(values))
+        client.publish("lab/photo/averge" % my_id, mean(values))
     time.sleep(0.01)
