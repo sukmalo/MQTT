678744eeb893c30ad70ab35ff987e84b29b3b783
sukmalo <114556628+sukmalo@users.noreply.github.com>
2022-12-20 14:51:04 +0800

    Update led_task_5.py

--- mqtt-master/led_task_5.py
+++ mqtt-master/led_task_5.py
@@ -8,7 +8,7 @@ import serial
 
 buffer = []
 
-my_id = 618
+my_id = 310
 
 mn = 100
 mx = 0
@@ -25,13 +25,13 @@ def on_message(client, userdata, message):
     topic = message.topic
     print(f"Received message on {topic}: {data}. min: {mn}, max: {mx}")
 
-    if topic == "lab/%s/photo/max" % my_id:
+    if topic == "lab/photo/max" % my_id:
         mx = int(data)
 
-    if topic == "lab/%s/photo/min" % my_id:
+    if topic == "lab/photo/min" % my_id:
         mn = int(data)
 
-    if topic == "lab/%s/photo/stream" % my_id:
+    if topic == "lab/photo/stream" % my_id:
         if int(data) < (mn + mx) / 2:
             ser.write(bytearray([ord('1')]))
         else:
@@ -51,9 +51,9 @@ except Exception:
 client.loop_start()
 
 print('Subscribing')
-client.subscribe("lab/%s/photo/stream" % my_id)
-client.subscribe("lab/%s/photo/min" % my_id)
-client.subscribe("lab/%s/photo/max" % my_id)
+client.subscribe("lab/photo/stream" % my_id)
+client.subscribe("lab/photo/min" % my_id)
+client.subscribe("lab/photo/max" % my_id)
 
 ser = get_connection('/dev/ttyUSB0')
 
